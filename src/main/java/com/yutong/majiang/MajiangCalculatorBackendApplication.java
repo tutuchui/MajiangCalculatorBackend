e2ff8679dd1d62ffc62b8e737cfd78131d654cc4
yutongwang <33520281+tutuchui@users.noreply.github.com>
2025-03-12 14:53:00 +0800

    initial commit

---
+++ src/main/java/com/yutong/majiang/MajiangCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yutong.majiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajiangCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yutong/majiang/MajiangCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutong.majiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
